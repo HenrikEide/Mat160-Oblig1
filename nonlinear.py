f184278b73ceafde5ee0c53f5144569c4f27bc8f
HenrikEide <Henrik225@gmail.com>
2021-09-27 21:26:12 +0200

    first commit

---
+++ nonlinear.py
@@ -0,0 +1,67 @@
+import math
+
+
+
+def newton(x_0, maxErr = 0.0000000001) -> str:
+    """Tilnærming til en rot og antall iterasjoner brukt 
+    gitt et startpunkt x_0 samt en nøyaktighet maxErr"""
+    x = x_0
+    iter = 0
+
+    while abs(f(x)) > maxErr:
+        x -= f(x)/fd(x)
+        iter += 1
+    
+    return f"{x:.10f} etter {iter} iterasjoner"
+
+def sekant(x_0, x_1, maxErr = 0.0000000001):
+    """Tilnærming til en rot og antall iterasjoner brukt 
+    gitt to startpunkt x_0, x_1 samt en nøyaktighet maxErr"""
+    iter = 0
+    x_next = 0
+    
+    while abs(f(x_next)) > maxErr:
+        iter += 1
+        x_curr = (x_0*f(x_1)-x_1*f(x_0))/(f(x_1)-f(x_0))
+        if f(x_0)*f(x_1) == 0:
+            break
+        x_0 = x_1
+        x_1 = x_curr
+        x_next = (x_0*f(x_1)-x_1*f(x_0))/(f(x_1)-f(x_0))
+    return f"{x_0:.10f} etter {iter} iterasjoner"
+
+def halvering(a, b, maxErr = 0.00001):
+    iter = 0
+    x = 0
+    while abs(f(x)) > maxErr:
+        x = (a+b)/2
+        if f(x)*b < 0:
+            a = x
+        else:
+            b = x
+        iter += 1
+    return f"{x:.10f} etter {iter} iterasjoner"
+
+if __name__ == "__main__":
+    def f(x : int = 0) -> float:
+        return x**3+10*math.cos(2*x)+math.log(x+11)
+
+    def fd(x : int = 0) -> float:
+        return 3*x**2-20*math.sin(2*x)+1/(x+11)
+    
+    print("Newtons metode:")
+    print(newton(-0.9))
+    print(newton(1))
+    print(newton(2))
+
+    print("\nSekantmetoden:")
+    print(sekant(-1, -0.9))
+    print(sekant(0.9, 1.1))
+    print(sekant(1.6, 2))
+
+    print("\nHalveringsmetoden")
+    print(halvering(-1, -0.9))
+    print(halvering(0.9, 1.1))
+    print(halvering(1.6, 2))
+
+
