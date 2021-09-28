ce82c339b6fba3a52ce35caddae74c7e96d070a7
HenrikEide <Henrik225@gmail.com>
2021-09-28 17:32:16 +0200

    nonlin kinda decent

--- nonlinear.py
+++ nonlinear.py
@@ -1,13 +1,11 @@
-import math
 import numpy as np
 
 
 def newton(x_0, maxErr = 0.0000000001) -> str:
     """Tilnærming til en rot og antall iterasjoner brukt 
-    gitt et startpunkt x_0 samt en nøyaktighet maxErr"""
+    gitt et startpunkt x_0 samt en toleranse maxErr"""
     x = x_0
     iter = 0
-
     while abs(f(x)) > maxErr:
         x -= f(x)/fd(x)
         iter += 1
@@ -16,21 +14,20 @@ def newton(x_0, maxErr = 0.0000000001) -> str:
 
 def sekant(x_0, x_1, maxErr = 0.0000000001):
     """Tilnærming til en rot og antall iterasjoner brukt 
-    gitt to startpunkt x_0, x_1 samt en nøyaktighet maxErr"""
+    gitt to startpunkt x_0, x_1 samt en toleranse maxErr"""
     iter = 0
-    x_next = 0; x_curr = math.inf
-    
-    while abs(x_curr-x_next) > maxErr:
+    x_next = 0
+    while abs(f(x_next)) > maxErr:
         iter += 1
         x_curr = (x_0*f(x_1)-x_1*f(x_0))/(f(x_1)-f(x_0))
-        if f(x_0)*f(x_1) == 0:
-            break
         x_0 = x_1
         x_1 = x_curr
         x_next = (x_0*f(x_1)-x_1*f(x_0))/(f(x_1)-f(x_0))
     return f"{x_0:.10f} etter {iter} iterasjoner"
 
 def halvering(a, b, maxErr = 0.0000000001):
+    """Tilnærming til en rot og antall iterasjoner brukt 
+    gitt et intervall (a,b) samt en toleranse maxErr"""
     iter = 0
     x = a
     while abs(f(x)) > maxErr:
@@ -45,10 +42,10 @@ def halvering(a, b, maxErr = 0.0000000001):
 
 if __name__ == "__main__":
     def f(x : int = 0) -> float:
-        return x**3+10*math.cos(2*x)+math.log(x+11)
+        return x**3+10*np.cos(2*x)+np.log(x+11)
 
     def fd(x : int = 0) -> float:
-        return 3*x**2-20*math.sin(2*x)+1/(x+11)
+        return 3*x**2-20*np.sin(2*x)+1/(x+11)
     
     print("Newtons metode:")
     print(newton(-0.9))
